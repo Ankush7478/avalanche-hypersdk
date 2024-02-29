ddd409043e30ca9b79b8e717c98492e8e69e1bf2
Ankush7478 <152309237+Ankush7478@users.noreply.github.com>
2024-02-29 23:51:54 +0530

    Update registry.go

--- registry/registry.go
+++ registry/registry.go
@@ -23,6 +23,11 @@ func init() {
 	errs.Add(
 		// When registering new actions, ALWAYS make sure to append at the end.
 		consts.ActionRegistry.Register(&actions.Transfer{}, actions.UnmarshalTransfer, false),
+                // TODO: register action: actions.CreateAsset
+                consts.ActionRegistry.Register(&actions.CreateAsset{}, actions.UnmarshalCreateAsset, false),
+
+                // TODO: register action: actions.MintAsset
+                consts.ActionRegistry.Register(&actions.MintAsset{}, actions.UnmarshalMintAsset, false),
 
 		// TODO: register action: actions.CreateAsset
 		// TODO: register action: actions.MintAsset
